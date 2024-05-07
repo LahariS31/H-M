0c1b4a1a6d61c333455ad24c3d9f306c2f5a2b33
LahariS31 <laharishree.s@hm.com>
2024-05-07 14:22:59 +0530

    Overriding

---
+++ OverRiding.java
@@ -0,0 +1,22 @@
+class Bank{
+    void roi(){
+        System.out.println("Normal intrest:10%");
+    }
+}
+class SBI extends Bank{
+
+}
+class Axis extends Bank{
+    void roi(){
+        System.out.println("Axis Bank overridden intrest:12%");
+    }
+}
+public class OverRiding {
+    public static void main(String[] args) {
+        SBI s=new SBI();
+        s.roi();
+        Axis a=new Axis();
+        a.roi();
+    }
+    
+}
