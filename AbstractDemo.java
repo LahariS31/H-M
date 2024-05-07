4f47c1dcf03c7862261c762d67a0162f60e718e9
LahariS31 <laharishree.s@hm.com>
2024-05-07 16:40:09 +0530

    update

---
+++ AbstractDemo.java
@@ -0,0 +1,43 @@
+abstract class Bank{
+    abstract void ATM();
+    abstract void KYC();
+    abstract void SB();
+}
+
+class SBI extends Bank{
+    void ATM(){
+        System.out.println("SBI ATM facility");
+    }
+    void KYC(){
+        System.out.println("kyc : Adhar only ");
+    }
+    void SB(){
+        System.out.println("Create SB Account");
+    }
+}
+
+class Axis extends Bank{
+    void ATM(){
+        System.out.println("Axis ATM facility");
+    }
+    void KYC(){
+        System.out.println("kyc : Pan only ");
+    }
+    void SB(){
+        System.out.println("Create SB Account optional");
+    }
+}
+public class AbstractDemo {
+
+    public static void main(String[] args) {
+        SBI s=new SBI();
+    s.ATM();
+     s.KYC();
+     s.SB();
+    Axis a=new Axis();
+    a.ATM();
+    a.KYC();
+    a.SB();
+    }
+    
+}
--- Assignment1.java
+++ Assignment1.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-class Emp {
+abstract class Emp {
     int age, id;
     String name;
     float Salary;
 
-    String desg ;
+    String desg;
 
     Emp() {
         Scanner sc = new Scanner(System.in);
@@ -18,13 +18,9 @@ class Emp {
         age = sc.nextInt();
 
     }
-    void raise(){
-        Salary=(float) (1.1*Salary);
-        System.out.println("------------------");
-        System.out.println("My Id:" + id + "My Name:" + name + "\n" + "My Age:" + age + "\n"  
-                + "\n" + "My Desgination:" + desg);
-        System.out.println("Updated Salary:" +(Salary));
-    }
+
+    abstract void raise();
+
     void display() {
         System.out.println("------------------");
         System.out.println("My Id:" + id + "My Name:" + name + "\n" + "My Age:" + age + "\n" + "My Salary:" + Salary
@@ -36,54 +32,66 @@ class Emp {
 
 class Clerk extends Emp {
 
-   
-   
-
     Clerk() {
         desg = "Clerk";
         Salary = 40000;
     }
+    void raise() {
+        Salary = (float) (1.1 * Salary);
+        System.out.println("------------------");
+        System.out.println("My Id:" + id + "My Name:" + name + "\n" + "My Age:" + age + "\n"
+                + "\n" + "My Desgination:" + desg);
+        System.out.println("Updated Salary:" + (Salary));
+    }
 
-   
 }
 
 class Manager extends Emp {
-
-     
- 
 
     Manager() {
         desg = "Manager";
         Salary = 100000;
     }
+    void raise() {
+        Salary = (float) (1.1 * Salary);
+        System.out.println("------------------");
+        System.out.println("My Id:" + id + "My Name:" + name + "\n" + "My Age:" + age + "\n"
+                + "\n" + "My Desgination:" + desg);
+        System.out.println("Updated Salary:" + (Salary));
+    }
 
-    
 }
 
 class Tester extends Emp {
-
-    
-    
 
     Tester() {
         desg = "Tester";
         Salary = 50000;
     }
+    void raise() {
+        Salary = (float) (1.1 * Salary);
+        System.out.println("------------------");
+        System.out.println("My Id:" + id + "My Name:" + name + "\n" + "My Age:" + age + "\n"
+                + "\n" + "My Desgination:" + desg);
+        System.out.println("Updated Salary:" + (Salary));
+    }
 
-    
 }
 
 class Dev extends Emp {
-
-     
-    
 
     Dev() {
         desg = "Developer";
         Salary = 50000;
     }
+    void raise() {
+        Salary = (float) (1.1 * Salary);
+        System.out.println("------------------");
+        System.out.println("My Id:" + id + "My Name:" + name + "\n" + "My Age:" + age + "\n"
+                + "\n" + "My Desgination:" + desg);
+        System.out.println("Updated Salary:" + (Salary));
+    }
 
-    
 }
 
 public class Assignment1 {
@@ -104,20 +112,19 @@ public class Assignment1 {
                     choice = sc.nextInt();
                     if (choice == 1) {
                         d = new Dev();
-                        
+
                     } else if (choice == 2) {
                         c = new Clerk();
-                        
+
                     } else if (choice == 3) {
                         m = new Manager();
-                        
+
                     } else if (choice == 4) {
-                         t = new Tester();
-                        
+                        t = new Tester();
+
                     }
 
                     else if (choice == 5) {
-                        
 
                     } else {
                         System.out.println("Invalid input");
@@ -126,64 +133,57 @@ public class Assignment1 {
                 } while (choice != 5);
 
             }
-        if (ch1==2){
-            do{
-            System.out.println("enter \n1)dev\n2)clerk\n3)Manager\n4)Tester\n5)exit");
-            choice = sc.nextInt();
-            if (choice == 1) {
-                
-                d.display();
-            } else if (choice == 2) {
-                
-                c.display();
-            } else if (choice == 3) {
-                
-                m.display();
-            } else if (choice == 4) {
-                
-                t.display();
+            if (ch1 == 2) {
+                do {
+                    System.out.println("enter \n1)dev\n2)clerk\n3)Manager\n4)Tester\n5)exit");
+                    choice = sc.nextInt();
+                    if (choice == 1) {
+
+                        d.display();
+                    } else if (choice == 2) {
+
+                        c.display();
+                    } else if (choice == 3) {
+
+                        m.display();
+                    } else if (choice == 4) {
+
+                        t.display();
+                    }
+
+                    else if (choice == 5) {
+
+                    } else {
+                        System.out.println("Invalid input");
+                    }
+
+                } while (choice != 5);
             }
+            if (ch1 == 3) {
 
-            else if (choice == 5) {
-                
+                System.out.println("enter \n1)dev\n2)clerk\n3)Manager\n4)Tester\n5)exit");
+                choice = sc.nextInt();
+                if (choice == 1) {
 
-            } else {
-                System.out.println("Invalid input");
+                    d.raise();
+                } else if (choice == 2) {
+
+                    c.raise();
+                } else if (choice == 3) {
+
+                    m.raise();
+                } else if (choice == 4) {
+
+                    t.raise();
+                }
+
+                else if (choice == 5) {
+
+                } else {
+                    System.out.println("Invalid input");
+                }
+
             }
-
-        
-
-        }while(choice!=5);
-    }
-        if (ch1==3){
-
-            System.out.println("enter \n1)dev\n2)clerk\n3)Manager\n4)Tester\n5)exit");
-            choice = sc.nextInt();
-            if (choice == 1) {
-                
-                d.raise();
-            } else if (choice == 2) {
-                
-                c.raise();
-            } else if (choice == 3) {
-                
-                m.raise();
-            } else if (choice == 4) {
-                
-                t.raise();
-            }
-
-            else if (choice == 5) {
-                
-
-            } else {
-                System.out.println("Invalid input");
-            }
-
-        
-
-        }
-        
 
         } while (ch1 != 4);
         sc.close();
