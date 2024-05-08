d22ce9d064ab4c9d3a18cd3bde9d1448b785bea8
LahariS31 <laharishree.s@hm.com>
2024-05-08 09:42:33 +0530

    update

--- AbstractDemo.java
+++ AbstractDemo.java
@@ -1,43 +1,50 @@
-abstract class Bank{
+abstract class Bank {
     abstract void ATM();
+
     abstract void KYC();
+
     abstract void SB();
 }
 
-class SBI extends Bank{
-    void ATM(){
+class SBI extends Bank {
+    void ATM() {
         System.out.println("SBI ATM facility");
     }
-    void KYC(){
+
+    void KYC() {
         System.out.println("kyc : Adhar only ");
     }
-    void SB(){
+
+    void SB() {
         System.out.println("Create SB Account");
     }
 }
 
-class Axis extends Bank{
-    void ATM(){
+class Axis extends Bank {
+    void ATM() {
         System.out.println("Axis ATM facility");
     }
-    void KYC(){
+
+    void KYC() {
         System.out.println("kyc : Pan only ");
     }
-    void SB(){
+
+    void SB() {
         System.out.println("Create SB Account optional");
     }
 }
+
 public class AbstractDemo {
 
     public static void main(String[] args) {
-        SBI s=new SBI();
-    s.ATM();
-     s.KYC();
-     s.SB();
-    Axis a=new Axis();
-    a.ATM();
-    a.KYC();
-    a.SB();
+        SBI s = new SBI();
+        s.ATM();
+        s.KYC();
+        s.SB();
+        Axis a = new Axis();
+        a.ATM();
+        a.KYC();
+        a.SB();
     }
-    
+
 }
